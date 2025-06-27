87ec8590b1abbdafa7ed8e31290f890452803326
Reitumetse Marline Mosehla <remosehjhb024@student.wethinkcode.co.za>
2025-06-27 12:05:45 +0200

    annotations for role

--- Models/Role.cs
+++ Models/Role.cs
@@ -1,6 +1,16 @@
-﻿namespace OmnitakSupportHub.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace OmnitakSupportHub.Models
 {
     public class Role
     {
+        [Key]
+        public int RoleID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string RoleName { get; set; } = null!;
+
+        [StringLength(200)]
+        public string? Description { get; set; }
     }
 }
