5dd00902c4c50b590b9c99e2b5e4a16809cb4f85
Kwazi-code <lehlohonolomashaya@gmail.com>
2025-06-27 12:58:36 +0200

    Authresult

---
+++ Models/AuthResult.cs
@@ -0,0 +1,12 @@
+﻿using OmnitakSupportHub.Models;    // for User
+
+namespace OmnitakSupportHub.Models
+{
+    public class AuthResult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = "";
+        public string? Token { get; set; }
+        public User? User { get; set; }
+    }
+}
