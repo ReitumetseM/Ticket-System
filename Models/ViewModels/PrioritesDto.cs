86773649c413e33aa78ddfb6e642c2e0e3ce4fbe
ReitumetseM <136059553+ReitumetseM@users.noreply.github.com>
2025-07-09 22:19:50 +0200

    Revert "created API controllers for managing entities"

--- Models/ViewModels/PrioritesDto.cs
+++
@@ -1,8 +0,0 @@
-﻿public class PriorityDto
-{
-    public int PriorityID { get; set; }
-    public string PriorityName { get; set; }
-    public string Label { get; set; }
-    public int Weight { get; set; }
-    public bool IsActive { get; set; }
-}
--- Models/ViewModels/TicketTimelineDto.cs
+++
@@ -1,7 +0,0 @@
-﻿public class TicketTimelineDto
-{
-    public int TimelineID { get; set; }
-    public int TicketID { get; set; }
-    public DateTime? ExpectedResolution { get; set; }
-    public DateTime? ActualResolution { get; set; }
-}
